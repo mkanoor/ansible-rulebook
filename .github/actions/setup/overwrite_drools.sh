fe88ec9f90509e94c768ffa11c3402a304694c72
Madhu Kanoor <mkanoor@redhat.com>
2023-06-02 16:37:05 -0400

    Switched to my repo

--- .github/actions/setup/overwrite_drools.sh
+++ .github/actions/setup/overwrite_drools.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 f=$(pip show drools_jpy | grep Location: | cut -d " " -f 2)/drools/jars/drools-ansible-rulebook-integration-runtime-1.0.2-SNAPSHOT.jar
-curl -L -o $f https://github.com/kiegroup/drools-ansible-rulebook-integration/releases/download/latest/drools-ansible-rulebook-integration-runtime-1.0.2-SNAPSHOT.jar
+#curl -L -o $f https://github.com/kiegroup/drools-ansible-rulebook-integration/releases/download/latest/drools-ansible-rulebook-integration-runtime-1.0.2-SNAPSHOT.jar
+curl -L -o $f https://raw.githubusercontent.com/mkanoor/debug_intermittent/main/drools-ansible-rulebook-integration-runtime-1.0.2-SNAPSHOT.jar
